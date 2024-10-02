66da40870f3e08b5d33aff4c3b65fc6488b0a1e1
LOSEARDES77 <loseardes77@gmail.com>
2024-10-02 09:12:10 +0200

    Update kitty.nix with improved settings

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -1,11 +1,15 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   programs.kitty = lib.mkForce {
     enable = true;
     settings = {
+      font_family = "CaskaydiaCove Nerd Font";
+      font_size = 15;
+      disable_ligatures = "never";
       confirm_os_window_close = 0;
       dynamic_background_opacity = true;
       enable_audio_bell = false;
@@ -13,6 +17,33 @@
       window_padding_width = 10;
       background_opacity = "0.5";
       background_blur = 5;
+      cursor_shape = "beam";
+      cursor = "#ffffff";
+      cursor_text_color = "#171717";
+      shell = "${pkgs.zsh}/bin/zsh";
+
+      # Charmful
+      background = "#171717";
+      foreground = "#b2b5b3";
+      selection_foreground = "#ffffff";
+      selection_background = "#313234";
+      color0 = "#373839";
+      color1 = "#e55f86";
+      color2 = "#00D787";
+      color3 = "#EBFF71";
+      color4 = "#51a4e7";
+      color5 = "#9077e7";
+      color6 = "#51e6e6";
+      color7 = "#e7e7e7";
+      color8 = "#313234";
+      color9 = "#d15577";
+      color10 = "#43c383";
+      color11 = "#d8e77b";
+      color12 = "#4886c8";
+      color13 = "#8861dd";
+      color14 = "#43c3c3";
+      color15 = "#b2b5b3";
+
       symbol_map = let
         mappings = [
           "U+23FB-U+23FE"
