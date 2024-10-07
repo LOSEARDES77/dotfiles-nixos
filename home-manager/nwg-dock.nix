02071eb1912894563e3cd33260570e53c76c2222
LOSEARDES77 <loseardes77@gmail.com>
2024-10-07 12:22:49 +0200

    fix

--- home-manager/nwg-dock.nix
+++ home-manager/nwg-dock.nix
@@ -7,7 +7,8 @@
     text = ''
       window {
         background: #171717;
-      	border-radius: 10px;
+        padding: 10px;
+      	border-radius: 15px;
       	border-style: none;
       	border-width: 1px;
       	/*border-color: rgba(156, 142, 122, 0.7)*/
@@ -15,20 +16,20 @@
 
       #box {
         /* Define attributes of the box surrounding icons here */
-        padding: 10px
+        padding: 10px;
       }
 
       #active {
       	/* This is to underline the button representing the currently active window */
       	border-bottom: solid 1px;
-      	border-color: rgba(255, 255, 255, 0.3)
+      	border-color: rgba(255, 255, 255, 0.3);
       }
 
       button, image {
       	background: none;
       	border-style: none;
       	box-shadow: none;
-      	color: #b2b5b3
+      	color: #b2b5b3;
       }
 
       button {
@@ -36,16 +37,17 @@
       	margin-left: 4px;
       	margin-right: 4px;
       	color: #b2b5b3;
-        font-size: 12px
+        font-size: 12px;
+        border-radius: 4px;
       }
 
       button:hover {
       	background-color: rgba(255, 255, 255, 0.15);
-      	border-radius: 2px;
+      	border-radius: 8px;
       }
 
       button:focus {
-      	box-shadow: none
+      	box-shadow: none;
       }
     '';
   };
