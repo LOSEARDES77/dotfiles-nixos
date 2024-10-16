96f82d4cf6fa6d75436786ffbbdfd7ff8f24e6ea
LOSEARDES77 <loseardes77@gmail.com>
2024-10-16 14:11:47 +0200

    add overlay

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -6,6 +6,23 @@
     ./scripts/vault.nix
   ];
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      cliphist = prev.cliphist.overrideAttrs (_oldAttrs: rec {
+        version = "0.6.1";
+
+        src = prev.fetchFromGitHub {
+          owner = "sentriz";
+          repo = "cliphist";
+          rev = "refs/tags/v${version}";
+          hash = "sha256-tImRbWjYCdIY8wVMibc5g5/qYZGwgT9pl4pWvY7BDlI=";
+        };
+
+        vendorHash = "sha256-gG8v3JFncadfCEUa7iR6Sw8nifFNTciDaeBszOlGntU=";
+      });
+    })
+  ];
+
   packages = with pkgs; {
     linux = [
       (mpv.override {scripts = [mpvScripts.mpris];})
