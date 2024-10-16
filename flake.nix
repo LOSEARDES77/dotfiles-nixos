30328b3ed4f9cab8eb2d4ba7131f2762ef63882f
LOSEARDES77 <loseardes77@gmail.com>
2024-10-16 10:07:39 +0200

    add overlay for cliphist

--- flake.nix
+++ flake.nix
@@ -21,6 +21,22 @@
         '';
       };
     };
+    overlays = [
+      (_: _final: prev: {
+        cliphist = prev.cliphist.overrideAttrs (_oldAttrs: rec {
+          version = "0.6.1";
+
+          src = prev.fetchFromGitHub {
+            owner = "sentriz";
+            repo = "cliphist";
+            rev = "refs/tags/v${version}";
+            hash = "sha256-tImRbWjYCdIY8wVMibc5g5/qYZGwgT9pl4pWvY7BDlI=";
+          };
+
+          vendorHash = "sha256-gG8v3JFncadfCEUa7iR6Sw8nifFNTciDaeBszOlGntU=";
+        });
+      })
+    ];
     # nixos config
     nixosConfigurations = {
       "loseardes77-laptop" = nixpkgs.lib.nixosSystem {
