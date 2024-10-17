546dd6d55f520be50d11576a4927b80c5eb5d0e9
LOSEARDES77 <loseardes77@gmail.com>
2024-10-17 08:37:21 +0200

    try remove overlay

--- flake.nix
+++ flake.nix
@@ -58,18 +58,5 @@
       url = "github:gokcehan/lf";
       flake = false;
     };
-
-    cliphist = prev: {
-      cliphist = prev.cliphist.overrideAttrs (_oldAttrs: rec {
-        version = "0.6.1";
-        src = prev.fetchFromGitHub {
-          owner = "sentriz";
-          repo = "cliphist";
-          rev = "refs/tags/v${version}";
-          hash = "sha256-tImRbWjYCdIY8wVMibc5g5/qYZGwgT9pl4pWvY7BDlI=";
-        };
-        vendorHash = "sha256-gG8v3JFncadfCEUa7iR6Sw8nifFNTciDaeBszOlGntU=";
-      });
-    };
   };
 }
