b22028735ba030c593b4b1333a73c3683608f08c
LOSEARDES77 <loseardes77@gmail.com>
2024-10-18 09:47:40 +0200

    fix

--- nixos/home.nix
+++ nixos/home.nix
@@ -9,7 +9,7 @@
   home = {
     sessionVariables = {
       QT_XCB_GL_INTEGRATION = "none"; # kde-connectQT_QT_
-      QT_QPA_PLATFORMTHEME = "qt6ct";
+      QT_QPA_PLATFORMTHEME = lib.mkDefault "qt6ct";
       NIXPKGS_ALLOW_UNFREE = "1";
       NIXPKGS_ALLOW_INSECURE = "1";
       BAT_THEME = "base16";
